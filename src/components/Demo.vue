575f5e53d3089f705c8345e507fa8bde8f550338
Brenden SJ Song <sj50179@gmail.com>
2022-07-08 22:51:13 +0900

    Modified app with Helper functions

---
+++ src/components/Demo.vue
@@ -0,0 +1,28 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div id="root">
+        <p>{{ originalPrice }}</p>
+        <p>{{ doublePrice }}</p>
+        <p>{{ triplePrice }}</p>
+    </div>
+</template>
+<script>
+import { mapGetters } from 'vuex'
+export default {
+    computed: {
+        ...mapGetters(['originalPrice', 'doublePrice', 'triplePrice'])
+        // originalPrice() {
+        //     return this.$store.gettes.originalPrice;
+        // },
+        // doublePrice() {
+        //     return this.$store.gettes.doublePrice;
+        // },
+        // triplePrice() {
+        //     return this.$store.gettes.triplePrice;
+        // }
+    }
+}
+</script>
+<style>
+    
+</style>
